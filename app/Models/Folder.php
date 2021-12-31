bc082fcb44ff46813f81841a19747e97cee9896e
James Brooks <james@alt-three.com>
2021-12-31 11:10:18 +0000

    Create migrations, models and update seeder

---
+++ app/Models/Folder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Folder extends Model
+{
+    use HasFactory;
+
+    public function todoItems(): HasMany
+    {
+        return $this->hasMany(TodoItem::class);
+    }
+}
---
+++ database/migrations/2021_12_29_155104_create_todo_items_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('folder_id');
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+}
